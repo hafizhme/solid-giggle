1676f36e95c39ebacd26f9b4e1250d9ff4a6fc1e
S Hafizh R Harsono <hafizhharsono@gmail.com>
2016-10-05 16:36:50 +0700

    Lexical Analyzer space separated

--- machine.py
+++ machine.py
@@ -94,9 +94,7 @@ class LexicalAnalyzer:
 		self.state = 0
 		tokenLexic = []
 		for self.cc in self.str:
-			print(self.cc)
 			self.decideState()
-			print(self.state)
 			if (0 < self.state) and (self.state <= 10):
 				tokenLexic.append(self.state)
 			elif (self.state is 99):
